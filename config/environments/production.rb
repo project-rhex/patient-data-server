b915ab0b57108b5504ca8899fba163f83afd5fee
Samuel Sayer <ssayer@mitre.org>
2012-01-04 15:29:54 -0500

    only force ssl in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 HdataServer::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -28,7 +28,7 @@ HdataServer::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug

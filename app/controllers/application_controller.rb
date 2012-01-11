5b2da761d076d28eae5cfa9c994a580aaee4d9c0
Samuel Sayer <ssayer@mitre.org>
2012-01-11 15:30:55 -0500

    add 404 for invalid record ids

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,6 @@ class ApplicationController < ActionController::Base
   def find_record
     record_id = params[:record_id] || params[:id]
     @record = Record.first(conditions: {medical_record_number: record_id})
+    render file: "public/404.html", :status => :not_found unless @record
   end
 end

1f2fbea5181bcc62a78c8e8b5458b2c33bb5187c
Doug Rand <drand@mitre.org>
2012-01-27 14:22:42 -0500

    Implemented atom feed for test results and getter for individual json test results. Implemented unit test for atom feed and individual test results

---
+++ app/controllers/results_controller.rb
@@ -0,0 +1,3 @@
+class ResultsController
+  # To change this template use File | Settings | File Templates.
+end
---
+++ lib/json_constants.rb
@@ -0,0 +1,12 @@
+# A place to create constants to avoid problems with misspelling literal strings
+module JSONConstants
+  AGE_AT_ONSET = "ageAtOnset"
+  CODE = "code"
+  CODES = "codes"
+  CODE_SYSTEM = "codeSystem"
+  COMMENT = "comment"
+  DESCRIPTION = "description"
+  PROBLEM_STATUS = "problemStatus"
+  STATUS = "status"
+  TIME = "time"
+end

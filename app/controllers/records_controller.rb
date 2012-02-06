e0ee44c4e1b648b36b0bcd10de0f3010f6915ca0
Robert Dingwell <bobd@mitre.org>
2012-02-06 10:28:03 -0500

    adding openid login to device view

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,5 +1,5 @@
 class RecordsController < ApplicationController
-    #before_filter :authenticate_user!
+  
   def index
     @records = Record.all
     
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,8 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 require 'devise/orm/mongoid'
-
+require 'omniauth-openid'
+require 'openid/store/filesystem'
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
@@ -198,7 +199,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
+  #config.omniauth :open_id, :store => OpenID::Store::Filesystem.new('/tmp'), :require => 'omniauth-openid'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
@@ -208,4 +209,5 @@ Devise.setup do |config|
   #   manager.intercept_401 = false
   #   manager.default_strategies(:scope => :user).unshift :some_external_strategy
   # end
+ 
 end

9734fdd707cd7872dfc4c3c0167b2cd749746b14
Andy Gregorowicz <andy@mitre.org>
2012-02-06 16:22:31 -0500

    Adding docs

--- lib/section_registry.rb
+++ lib/section_registry.rb
@@ -8,6 +8,14 @@ class SectionRegistry
     @extensions = []
   end
   
+  # Adds an hData section to the registry
+  # @param [String] path the location in the web application where this section is registered.
+  #                      For example: 'results'
+  # @param [String] extension_id the URI that identifies the type of the section
+  # @param [String] name a human readable name for the section
+  # @yield [Hash, Hash] If a block is provided, hashes are given that can be used to add importers
+  #                     and exporters. The Hash key should be a String representing a Mime Type. 
+  #                     The value should be the importer/exporter.
   def add_section(path, extension_id, name)
     importers = {}
     exporters = {}
@@ -16,6 +24,9 @@ class SectionRegistry
                                   importers: importers, exporters: exporters)
   end
   
+  # Finds an extension based on the path passed in
+  # @param [String] path the path of the extension you are looking for
+  # @return an object that has the name, path, extension, importers and exporters for the path
   def extension_from_path(path)
     @extensions.find {|e| e.path == path}
   end

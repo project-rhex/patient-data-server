76f8d86b99a910a06786ca5d45e635280f4850d6
Marshall Huss <mhuss@mitre.org>
2012-02-15 10:27:27 -0500

    Removed config.assets.enabled = false in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,6 @@ HdataServer::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-  config.assets.enabled = false
 
   # Generate digests for assets URLs
   config.assets.digest = true

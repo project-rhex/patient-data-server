a11333cc48ff7c3ab7a5d262471977da5d020ca4
Doug Rand <drand@mitre.org>
2012-02-21 15:40:12 -0500

    Make bullet image more like mockup. Improve coverage in record_helper_test

--- test/unit/helpers/record_helper_test.rb
+++ test/unit/helpers/record_helper_test.rb
@@ -12,11 +12,25 @@ class RecordHelperTest < ActionView::TestCase
 
   test "section_enumerator" do
     rec = FactoryGirl.create(:record, :with_lab_results)
+    r2 = FactoryGirl.create(:lab_result)
+    r2.time = Time.now.to_i
+    rec.results << r2
     i = 0
     section_enumerator(rec, :results, (Time.now - 3000000000)) do |x|
       i += 1
     end
     assert i > 0
+  end
+
+  test "section_title" do
+    assert_equal "<h2>Lab Results</h2>", section_title(:results)
+  end
+
+  test "section_history_link" do
+    rec = FactoryGirl.create(:record, :with_lab_results)
+
+    assert_equal "<div class='history_link'><a href='/records/5/results'><<&nbsp;Past Lab Results</a></div>",
+      section_history_link(rec, :results)
   end
 
   test "most_recent_vital_date" do
@@ -46,6 +60,9 @@ class RecordHelperTest < ActionView::TestCase
     rec.vital_signs.concat([ vs1, vs2, vs3 ])
     v = latest_matching_vital rec, 'ldl'
     assert_equal "<span class='lab_value'>127&nbps;(mg/dL)</span>", v
+
+    v = latest_matching_vital rec, 'xyz'
+    assert_equal "", v
   end
 
   test "sex method" do
@@ -57,4 +74,21 @@ class RecordHelperTest < ActionView::TestCase
     r = FactoryGirl.create(:lab_result)
     assert_equal "<span class='lab_value'>127&nbps;(mg/dL)</span>", show_value(r.value)
   end
+
+  test "format a value 2" do
+    assert_equal "<span class='lab_value'>127</span>", show_value({'scalar' => 127})
+  end
+
+  test "format a value 3" do
+    assert_equal "<span class='lab_value'>127.12</span>", show_value({'scalar' => 127.12345})
+  end
+
+  test "format a value 4" do
+    assert_equal "<span class='lab_value'>127.12</span>", show_value({'scalar' => "127.12345"})
+  end
+
+  test "format a value 5" do
+    assert_equal "<span class='lab_value'>0</span>", show_value({})
+  end
+
 end

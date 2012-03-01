25826ffc26e82e6c48636885e7d9478f253ad535
Robert Dingwell <bobd@mitre.org>
2012-03-01 13:26:42 -0500

    restricting access to root.xml to only get requests

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ HdataServer::Application.routes.draw do
 
   #
   match "records/:id" => "records#show", :as => "root_feed", :format => :atom
-  match "records/:id/root.xml" => "records#root", :as => :root_document, :format => :xml
+  match "records/:id/root.xml" => "records#root", :as => :root_document, :format => :xml, :method=> :get
   match "records/:record_id/:section" => "entries#index", :as => :section_feed, :format => :atom, :method => :get
   match "records/:record_id/:section/:id" => "entries#show", :as => :section_document, :method => :get
   match "records/:record_id/:section" => "entries#create", :as => :new_section_document, :method => :post

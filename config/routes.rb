c2b0ad05205c8a0cbfc47c704dc54d4c5c1c8ecd
Ganley <gganley@mitre.org>
2012-04-04 14:35:14 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 HdataServer::Application.routes.draw do
 
+  ##
   resources :ref_consult_requests
   match "ref_consult_requests/new/:id" => "ref_consult_requests#new", :as => :new_ref_consult_request_patient, :method => :get
 

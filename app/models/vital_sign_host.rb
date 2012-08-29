6a2831de626783d8d8b1084fcf3aecfd17bfac7f
Andy Gregorowicz <andy@mitre.org>
2012-08-29 10:03:29 -0400

    Adding code to generate the authorization request url

--- app/models/vital_sign_host.rb
+++ app/models/vital_sign_host.rb
@@ -5,4 +5,15 @@ class VitalSignHost
   field :client_secret, :type => String
   
   has_many :vital_sign_feeds
+  
+  def authorization_request_url(redirect_uri, use_https=false)
+    query = {'client_id' => client_id, 'client_secret' => client_secret,
+             'response_type' => 'code', 'redirect_uri' => redirect_uri}.to_query
+    path = '/authorize'
+    if use_https
+      URI::HTTPS.build(host: hostname, path: path, query: query)      
+    else
+      URI::HTTP.build(host: hostname, path: path, query: query)
+    end
+  end
 end
--- test/unit/vital_sign_host_test.rb
+++ test/unit/vital_sign_host_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class VitalSignHostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "generating the authorization request url" do
+    vsh = FactoryGirl.create(:vital_sign_host)
+    url = vsh.authorization_request_url('http://splat.blat.com')
+    assert_equal 'foo.bar.com', url.host
+    assert url.query.include? 'client_id=1234'
+  end
 end
